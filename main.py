1248156cd16a9916a3c02880e3a857fde339f7a7
laricanonga <francisco.riffo10@inacapmail.cl>
2026-09-17 11:32:43 -0300

    avance clase paciente probada

---
+++ main.py
@@ -0,0 +1,10 @@
+from paciente import Paciente
+
+def main():
+    # creando objeto paciente
+    p1=Paciente("22.222.113-5","Pedro Pascal",300000,"Fonasa")
+    print(p1)
+
+
+if __name__=="__main__":
+    main()
--- paciente.py
+++ paciente.py
@@ -1,4 +1,6 @@
 class Paciente: 
+
+    PREVISIONES_VALIDAS:set[str]={"Fonasa","Isapre","Particular","Otro"}
     def __init__(self, rut:str, nombre:str, edad:int,prevision:str):
         self.rut = rut 
         self.nombre=nombre
@@ -14,6 +16,37 @@ class Paciente:
         self._rut = rut
 
 
+    @property
+    def nombre(self)-> str: 
+        return self._nombre
+
+    @nombre.setter 
+    def nombre(self,nombre:str)-> None:
+        self._nombre = nombre 
+    
+
+    @property
+    def edad(self)-> int:
+        return self._edad 
+
+    @edad.setter 
+    def edad(self,edad:int)-> None:
+        self._edad = edad
+
+    @property
+    def prevision(self)->str:
+        return self._prevision
+        
+    @prevision.setter
+    def prevision(self,prevision:str)-> None:
+        self._prevision = prevision 
+
+    def __str__(self)->str:
+        return f"Informacion del pacienter:\nRUT: {self.rut}\nNombre: {self.nombre}\nEdad:{self.edad}\nPrevision: {self.prevision}"
+
+    def __repr__(self)-> str:
+        return f"Paciente(rut='(self.rut)', nombre='{self.nombre}', edad={self.edad}, prevision='{self.prevision}')"
+    
 
 
 
